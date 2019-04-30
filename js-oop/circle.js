b65c47a614f517bde42007d0b12118328c7f24a0
Austin Lords <austinlords@outlook.com>
2019-04-30 11:49:44 -0700

    js-oop lesson complete

---
+++ js-oop/circle.js
@@ -0,0 +1,14 @@
+// Implementation Detail (not exported)
+const _radius = new WeakMap();
+
+// Public Interface
+ export class Circle {
+  constructor(radius) {
+    _radius.set(this, radius);
+  }
+
+  draw() {
+    console.log('Circle with radius ' + _radius.get(this));
+  }
+}
+
