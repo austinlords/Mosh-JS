982e8d5f85131f8830007d07a2c508411983b5e2
Austin Lords <austinlords@outlook.com>
2019-05-13 21:02:12 -0700

    Vidly genres added to database

--- node/vidly/index.js
+++ node/vidly/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
+const mongoose = require('mongoose');
+const debug = require('debug')('debug');
+
+mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true })
+  .then(() => debug('connected to MongoDB...'))
+  .catch(err => debug('FAILED CONNECTION TO MongoDB...', err));
 
 app.use(express.json());
 app.use('/api/genres', genres);
@@ -8,7 +14,6 @@ app.use('/api/genres', genres);
 app.get('/', (req, res) => {
   res.send('This is the home page. Welcome!');
 })
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- node/vidly/routes/genres.js
+++ node/vidly/routes/genres.js
@@ -1,68 +1,76 @@
 const express = require('express');
 const router = express.Router();
 const Joi = require('@hapi/joi');
+const mongoose = require('mongoose');
+const debug = require('debug')('genres');
 
-let genres = [
-  { id: 1, name: 'horror' },
-  { id: 2, name: 'comedy' },
-  { id: 3, name: 'drama' },
-  { id: 4, name: 'action' },
-  { id: 5, name: 'children' }
-];
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    minlength: 2,
+    maxlength: 50 
+  }
+});
 
-function validateGenre(genre) {
-  let schema = {
-    name: Joi.string().min(3).required()
-  };
-  return Joi.validate(genre, schema);
+const Genre = mongoose.model('Genre', schema);
+
+async function createGenre(name) {
+  let genre = new Genre({ name: name });
+  let result = await genre.save();
+  debug(result);
 }
 
-router.get('/', (req, res) => {
-  res.send(genres);
+router.get('/', async (req, res) => {
+  const genres = await Genre.find().sort('name');
+  res.send(genres)
 })
 
-router.get('/:id', (req, res) => {
-  let genre = genres.find(obj => obj.id == parseInt(req.params.id));
+router.get('/:id', async (req, res) => {
+  const genre = await Genre.findById(req.params.id);
+
   if (!genre) return res.status(404).send('Genre not found');
   
   res.send(genre);
 })
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   let { error } = validateGenre(req.body);
   if (error) return res.status(400).send(error.details[0].message);
   
-  let genre = genres.find(obj => obj.name === req.body.name);
-  if (genre) return res.status(400).send('This genre already exists');
-  
-  genre = {
-    id: genres[genres.length-1].id + 1,
-    name: req.body.name
+  const newGenre = new Genre({ name: req.body.name });
+  const result = await newGenre.save();
+
+  res.send(result); 
+})
+
+router.put('/:id', async (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true })
+
+  if (!genre) return res.status(404).send('Genre not found');
+
+  res.send(genre);
+})
+
+router.delete('/:id', async (req, res) => {
+  const genre = await Genre.findByIdAndRemove(req.params.id);
+
+  if (!genre) return res.status(404).send('Genre not found');
+
+  res.send(genre)
+})
+
+function validateGenre(genre) {
+  const schema = {
+    name: Joi.string().min(3).required()
   };
 
-  genres.push(genre);
-  res.send(genre); 
-})
-
-router.put('/:id', (req, res) => {
-  let { error } = validateGenre(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-  
-  let genre = genres.find(genre => genre.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send('Genre not found');
-
-  genre.name = req.body.name;
-  res.send(genre);
-})
-
-router.delete('/:id', (req, res) => {
-  let genre = genres.find(genre => genre.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send('Genre not found');
-
-  let index = genres.indexOf(genre);
-  genres.splice(index, 1);
-
-  return res.send(genre)
-})
+  return Joi.validate(genre, schema);
+}
 
 module.exports = router;
